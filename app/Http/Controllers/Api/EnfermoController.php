e77265f2598c6b3baf5190541e9953d3722f888a
GiordaniSantos <dani.santossilveira@hotmail.com>
2026-04-01 18:14:13 -0300

    ajustado para novo padrão enfermo controller

--- app/Http/Controllers/Api/EnfermoController.php
+++ app/Http/Controllers/Api/EnfermoController.php
@@ -1,103 +1,20 @@
 <?php
-
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Http\Requests\CommonRequest;
 use App\Http\Resources\CommonListResource;
-use App\Models\Enfermo;
-use Illuminate\Http\Request;
-use Illuminate\Http\UploadedFile;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\URL;
-use Illuminate\Support\Str;
-use Carbon\Carbon;
+use App\Services\EnfermoService;
 
-class EnfermoController extends Controller
+class EnfermoController extends BaseCrudController
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    protected $service; 
+
+    protected string $resourceCollection = CommonListResource::class; 
+    
+    protected string $formRequest = CommonRequest::class;
+
+    public function __construct(EnfermoService $service)
     {
-        $id_usuario = request('id_usuario');
-        $query = Enfermo::query()
-            ->where('id_usuario', $id_usuario)
-            ->orderBy('created_at', 'desc')
-            ->take(15)
-            ->get();
-
-        return CommonListResource::collection($query);
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $request->validate(Enfermo::rules(), Enfermo::feedback());
-        $enfermo = new Enfermo();
-        $enfermo->id_usuario = $request->input('id_usuario');
-        $enfermo->save();
-
-        return response()->json($enfermo, 201);
-    }
-
-       /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Enfermo  $enfermo
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        $id_usuario = request('id_usuario');
-        $enfermo = Enfermo::where('id', $id)->where('id_usuario', $id_usuario)->first();
-        if($enfermo === null){
-            return response()->json(['erro' => 'Recurso pesquisado não existe.'], 404);
-        }
-        return response()->json($enfermo, 200);
-    }
-
-     /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Enfermo  $enfermo
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        $id_usuario = request('id_usuario');
-        $enfermo = Enfermo::where('id', $id)->where('id_usuario', $id_usuario)->first();
-        $request->validate(Enfermo::rules(), Enfermo::feedback());
-        $enfermo->id_usuario = $id_usuario;
-        $enfermo->nome = $request->nome;
-        $enfermo->created_at = Carbon::parse($request->created_at)->setTimezone('America/Sao_Paulo');
-        $enfermo->save();
-
-        return response()->json($enfermo, 200);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param \App\Models\Enfermo $enfermo
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        $id_usuario = request('id_usuario');
-        $enfermo = Enfermo::where('id', $id)->where('id_usuario', $id_usuario)->first();
-        if($enfermo === null){
-            return response()->json(['erro' => 'Recurso pesquisado não existe.'], 404);
-        }
-        $enfermo->delete();
-
-        return response()->json(['msg' => 'Registro deletado com sucesso!'], 200);
+        parent::__construct($service);
     }
 }
